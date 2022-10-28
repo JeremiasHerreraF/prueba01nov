44ac59c023277a9efe6c2b03cd89eacd26b9be42
Miguel Zolk <zolkmiguel@gmail.com>
2022-10-28 17:36:09 -0300

    healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service';
 import { MiddlewareModule } from './middleware/middleware.module';
 import { AuthModule } from './auth/auth.module';
 import { AccountsModule } from './accounts/accounts.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LanaHttpModule } from './accounts/tpp/lana.tpp';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -13,6 +15,24 @@ import { LanaHttpModule } from './accounts/tpp/lana.tpp';
       envFilePath: ['.env'],
       isGlobal: true
     }),
+    MongooseModule.forRoot(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    {
+      ...JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+            audience: process.env.JWT_AUDIENCE,
+          },
+        }),
+      }),
+      global: true,
+    },
     LanaHttpModule,
     MiddlewareModule, AuthModule, AccountsModule],
   controllers: [AppController],
